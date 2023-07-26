1e464c57140bb0a00f4f8f8c24c48a5dbf0639f4
Jose <jmgomezdevel@gmail.com>
2023-07-26 23:08:53 +0200

    Deleting unused imports

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { EventInfo } from 'src/models/event-info';
 import { BehaviorSubject } from 'rxjs';
 
